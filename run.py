5bbe70fe1d7ac6f9417ab7985823a577c21205ff
svaz18 <svaz18@hotmail.com>
2025-08-06 10:18:52 +0100

    Add wieghted average function and test

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('adviser_survey')
 
 # Constants 
-Questions = [
+QUESTIONS = [
     "Professionalism",
     "Clarity of Advice",
     "Responsiveness",
@@ -24,7 +24,7 @@ Questions = [
     "Overall Experience"
 ]
 
-Weights = [1, 1.3, 0.9, 1.1, 1.1, 0.9, 1, 1.3]
+WEIGHTS = [1, 1.3, 0.9, 1.1, 1.1, 0.9, 1, 1.3]
 
 #Functions 
 
@@ -35,7 +35,7 @@ def get_survey_data():
 
     print("\n Adviser Survey: Please rate the following categories from 1 to 10 \n")
     ratings = []
-    for q in Questions:
+    for q in QUESTIONS:
         while True:
             try:
                 value = int(input(f"{q}: "))
@@ -72,13 +72,21 @@ def calculate_averages(data):
     transposed = list(zip(*data))
     return [round(sum(col) / len(col), 1) for col in transposed]
 
-
+def calculate_weighted_average(averages):
+    """
+    Applies weights to scores to calculate weighted averages for each question
+    """
+    weighted_total = sum(avg * weight for avg, weight in zip(averages,WEIGHTS))
+    total_weights = sum(WEIGHTS)
+    return round(weighted_total / total_weights, 2)
     
 def main():
     ratings = get_survey_data()
     update_worksheet(ratings, "responses")
 
     all_data = get_all_responses()
+    averages = calculate_averages(all_data)
+    weighted_avg = calculate_weighted_average(averages)
     
 
 if __name__ == "__main__":
